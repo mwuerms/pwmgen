ee89278344814afeaa175d0a1f99d9a90e898bd8
Martin Egli <eglidev@gmx.net>
2022-06-19 20:50:29 +0200

    pwm setup 2
    deleted disp_draw_elements

--- disp_draw.c
+++ disp_draw.c
@@ -6,36 +6,38 @@
 
 /* - includes --------------------------------------------------------------- */
 #include "disp_draw.h"
-#include "disp_draw_elements.h"
 #include "lcd.h"
 #include "project.h"
 #include "version.h"
 #include "wdt.h"
 #include <stdint.h>
 
-void disp_draw_init(void) {
+void disp_draw_init(void)
+{
   // gpios
   // vars
   lcd_init(LCD_DISP_ON); // init lcd and turn on
   lcd_charMode(DOUBLESIZE);
 }
-void disp_draw_clear(void) {
+void disp_draw_clear(void)
+{
   lcd_clrscr();
   lcd_display();
 }
-#define TEXT_INFO "INFO InFo"
-struct {
+struct
+{
   int8_t step;
   uint8_t pox_x;
   uint8_t repeate;
 } info_ctrl;
-void disp_draw_info(void) {
+void disp_draw_info(void)
+{
   // lcd_charMode(DOUBLESIZE);
-  lcd_puts("Hello World");     // put string from RAM to display (TEXTMODE) or
-                               // buffer (GRAPHICMODE)
-  lcd_gotoxy(0, 2);            // set cursor to first column at line 3
-  lcd_puts_p(PSTR(TEXT_INFO)); // puts string form flash to display (TEXTMODE)
-                               // or buffer (GRAPHICMODE)
+  lcd_puts("Hello World"); // put string from RAM to display (TEXTMODE) or
+                           // buffer (GRAPHICMODE)
+  lcd_gotoxy(0, 2);        // set cursor to first column at line 3
+  lcd_puts_p("ab");        // puts string form flash to display (TEXTMODE)
+                           // or buffer (GRAPHICMODE)
   lcd_drawLine(0, 4, MAX_X, 4, WHITE);
   /*SSD1306_SetPosition (7, 1);
   SSD1306_DrawString (PROGRAM_NAME);
@@ -73,7 +75,8 @@ void disp_draw_info(void) {
                         EV_DISPLAY_UPDATE_INFO);
 }
 
-uint8_t disp_draw_update_info(void) {
+uint8_t disp_draw_update_info(void)
+{
   /*SSD1306_ClearArea (30-1,60-1,ROW_TO_Y(3)+0, ROW_TO_Y(3)+8+1);
   if(info_ctrl.step >= +1) {
       info_ctrl.pox_x += info_ctrl.step;
@@ -111,10 +114,14 @@ uint8_t disp_draw_update_info(void) {
 // DOUBLESIZE: x: 10 chars in 128 pixel, y: 4 lines in 64 pixel
 uint8_t font_size_width[] = {0, 128 / 21, 128 / 10};
 uint8_t font_size_height[] = {0, 64 / 8, 64 / 4};
-void disp_draw_mark_position(uint8_t cx, uint8_t cy, uint8_t size) {
-  if (size != DOUBLESIZE) {
+void disp_draw_mark_position(uint8_t cx, uint8_t cy, uint8_t size)
+{
+  if (size != DOUBLESIZE)
+  {
     size = NORMALSIZE;
-  } else {
+  }
+  else
+  {
     cy = cy / 2;
   }
   uint8_t x0, y0, x1, y1;
@@ -126,27 +133,44 @@ void disp_draw_mark_position(uint8_t cx, uint8_t cy, uint8_t size) {
   // lcd_drawRect(7*8-2, 4*8, 8*8+3, 4*8-1, WHITE);
 }
 
-struct {
+static enum pwm_setup_steps {
+  STEP_FREQ = 0,
+  STEP_DUTY,
+  STEP_SWEEP,
+};
+
+#define PWM_STATUS_ON _BV(0) // =1: ON, =0: OFF
+static char pwm_status_text[][4] = {"OFF", "ON"};
+
+static struct
+{
   uint16_t freq;    // 0 ... 10000  (0 ... 10000 Hz)
   uint8_t freq_pos; // 0 ... 3 (1, 10, 100, 1000)
   uint16_t duty;    // 0 ... 1000 (0.0 ... 100.0 %)
   uint8_t duty_pos; // 0, 2, 3 (0.1, 1.0, 10.0)
+  uint8_t status;
+  enum pwm_setup_steps step;
 } pwm_settings = {
     .freq = 10,
     .freq_pos = 0,
     .duty = 500,
     .duty_pos = 0,
+    .status = 0,
+    .step = 1,
 };
 #define FREQ_POS_0 (5)
-#define DUTY_POS_0 (5)
+#define DUTY_POS_0 (6)
 
 #define LINE_SIZE (23)
 static char line[LINE_SIZE + 1]; // +1: '\0' at the end
 
-void stringcopyn(char *to, char *from, uint8_t max_size) {
+void stringcopyn(char *to, char *from, uint8_t max_size)
+{
   uint8_t n;
-  for (n = 0; n < max_size; n++) {
-    if (from[n] == '\0') {
+  for (n = 0; n < max_size; n++)
+  {
+    if (from[n] == '\0')
+    {
       // found end
       break;
     }
@@ -155,97 +179,146 @@ void stringcopyn(char *to, char *from, uint8_t max_size) {
   to[n] = '\0'; // termination
 }
 
-static freq_to_line(void) {
-  uint16_t divisor[] = {1, 10, 100, 1000};
-  uint16_t f;
-  uint8_t digit, pos, space;
-  if (pwm_settings.freq == 10000) {
-    // special case
-    stringcopyn(line, " 10000 Hz", LINE_SIZE);
-    return;
-  }
-  stringcopyn(line, "     0 Hz", LINE_SIZE);
-  f = pwm_settings.freq;
+static char u16_str[8];
+static char *uint16_t_to_str(uint16_t u16, uint8_t nb_digits)
+{
+  uint16_t divisor[] = {1, 10, 100, 1000, 10000};
+  uint8_t digit, digit_pos, str_pos, space;
   space = 1;
-  for (pos = 3; pos != 0; pos--) {
-    digit = (uint8_t)(f / divisor[pos]);
-    if ((digit == 0) && (space == 1)) {
+  str_pos = 0;
+  for (digit_pos = nb_digits - 1; digit_pos != 0; digit_pos--)
+  {
+    digit = (uint8_t)(u16 / divisor[digit_pos]);
+    if ((digit == 0) && (space == 1))
+    {
       // skip
+      u16_str[str_pos++] = ' ';
       continue;
-    } else {
+    }
+    else
+    {
       space = 0;
-      line[FREQ_POS_0 - pos] = digit + '0';
-      f = f - (digit * divisor[pos]);
+      u16_str[str_pos++] = digit + '0';
+      u16 = u16 - (digit * divisor[digit_pos]);
     }
   }
-  digit = (uint8_t)f;
-  line[FREQ_POS_0 - pos] = digit + '0';
+  digit = (uint8_t)u16;
+  u16_str[str_pos++] = digit + '0';
+  u16_str[str_pos++] = '\0';
+  return u16_str;
 }
 
-static duty_to_line(void) {
-  uint16_t divisor[] = {1, 10, 100};
-  uint16_t d;
-  uint8_t digit, pos, space;
-  if (pwm_settings.duty == 1000) {
+static void frequency_to_line(void)
+{
+  char *res_str;
+  if (pwm_settings.freq == 10000)
+  {
     // special case
-    stringcopyn(line, " 100.0 %", LINE_SIZE);
+    stringcopyn(line, "  10000 Hz", LINE_SIZE);
     return;
   }
-  stringcopyn(line, "   0.0 %", LINE_SIZE);
-  d = pwm_settings.duty;
-  space = 1;
-  for (pos = 2; pos != 0; pos--) {
-    digit = (uint8_t)(d / divisor[pos]);
-    if ((digit == 0) && (space == 1)) {
-      // skip
-      continue;
-    } else {
-      space = 0;
-      line[DUTY_POS_0 - pos - 1] = digit + '0'; // -1: '.' in 0.0
-      d = d - (digit * divisor[pos]);
-    }
-  }
-  digit = (uint8_t)d;
-  line[DUTY_POS_0] = digit + '0';
+  stringcopyn(line, "      0 Hz", LINE_SIZE);
+  res_str = uint16_t_to_str(pwm_settings.freq, 4);
+  stringcopyn(&line[3], res_str, LINE_SIZE);
+  stringcopyn(&line[3 + 4], " Hz", LINE_SIZE);
 }
 
-void disp_draw_pwm_setup(void) {
-  // text 1st
+static void frequency_small_to_line(void)
+{
+  char *res_str;
+  if (pwm_settings.freq == 10000)
+  {
+    // special case
+    stringcopyn(line, "Freq:10000Hz", LINE_SIZE);
+    return;
+  }
+  stringcopyn(line, "Freq:    0Hz", LINE_SIZE);
+  res_str = uint16_t_to_str(pwm_settings.freq, 4);
+  stringcopyn(&line[6], res_str, LINE_SIZE);
+  stringcopyn(&line[6 + 4], " Hz", LINE_SIZE);
+}
+
+static void duty_to_line(void)
+{
+  char *res_str;
+  if (pwm_settings.duty == 1000)
+  {
+    // special case
+    stringcopyn(line, "  100.0 %", LINE_SIZE);
+    return;
+  }
+  stringcopyn(line, "    0.0 %", LINE_SIZE);
+  res_str = uint16_t_to_str(pwm_settings.duty, 3);
+  stringcopyn(&line[3], res_str, LINE_SIZE);
+  stringcopyn(&line[3 + 2], ".", LINE_SIZE);
+  stringcopyn(&line[3 + 3], &res_str[2], LINE_SIZE);
+  stringcopyn(&line[3 + 4], " %", LINE_SIZE);
+}
+
+static void duty_small_to_line(void)
+{
+  char *res_str;
+  if (pwm_settings.duty == 1000)
+  {
+    // special case
+    stringcopyn(line, "Duty: 100.0%", LINE_SIZE);
+    return;
+  }
+  stringcopyn(line, "Duty:   0.0%", LINE_SIZE);
+  res_str = uint16_t_to_str(pwm_settings.duty, 3);
+  stringcopyn(&line[6], res_str, LINE_SIZE);
+  stringcopyn(&line[6 + 2], ".", LINE_SIZE);
+  stringcopyn(&line[6 + 3], &res_str[2], LINE_SIZE);
+  stringcopyn(&line[6 + 4], " %", LINE_SIZE);
+}
+
+void disp_draw_pwm_setup(void)
+{
+  // pwm_settings.freq = 36;
+  // pwm_settings.duty = 567;
+
+  // 1st: status line
   lcd_charMode(DOUBLESIZE);
   lcd_gotoxy(0, 0);
-  lcd_puts("Frequency");
-
-  lcd_gotoxy(0, 2);
-  // lcd_puts(" 10000 Hz");
-  pwm_settings.freq = 9020;
-  freq_to_line();
+  lcd_puts(pwm_status_text[(pwm_settings.status & PWM_STATUS_ON)]);
+  lcd_charMode(NORMALSIZE);
+  lcd_gotoxy(8, 0);
+  frequency_small_to_line();
+  lcd_puts(line);
+  lcd_gotoxy(8, 1);
+  duty_small_to_line();
   lcd_puts(line);
 
-  lcd_gotoxy(0, 4);
-  lcd_puts("Duty cycle");
-
-  lcd_gotoxy(0, 6);
-  // lcd_puts(" 100.0 %");
-  pwm_settings.duty = 902;
-  duty_to_line();
-  lcd_puts(line);
-
-  lcd_gotoxy(0, 2);
-  lcd_puts(">");
-
-  // lcd_drawRect(7*8-2, 4*8, 8*8+3, 4*8-1, WHITE);
-  // disp_draw_mark_position(5, 4, DOUBLESIZE);
-  disp_draw_mark_position((FREQ_POS_0 - pwm_settings.freq_pos), 2, DOUBLESIZE);
-  disp_draw_mark_position((DUTY_POS_0 - pwm_settings.duty_pos), 6, DOUBLESIZE);
-
-  // testing
-  /*lcd_gotoxy(0, 0);
-  lcd_puts("0123456789ABCDEF");
-  disp_draw_mark_position(0, 0, DOUBLESIZE);
-  disp_draw_mark_position(2, 0, DOUBLESIZE);
-  disp_draw_mark_position(4, 0, DOUBLESIZE);
-  disp_draw_mark_position(6, 0, DOUBLESIZE);
-  disp_draw_mark_position(8, 0, DOUBLESIZE);
-*/
+  // 2nd: settings
+  lcd_charMode(DOUBLESIZE);
+  switch (pwm_settings.step)
+  {
+  case STEP_FREQ:
+    lcd_gotoxy(0, 2);
+    lcd_puts("Frequency");
+    lcd_gotoxy(0, 4);
+    frequency_to_line();
+    lcd_puts(line);
+    lcd_gotoxy(0, 6);
+    lcd_puts("Duty cycle");
+    lcd_gotoxy(0, 4);
+    lcd_puts(">");
+    disp_draw_mark_position((FREQ_POS_0 - pwm_settings.freq_pos), 4, DOUBLESIZE);
+    break;
+  case STEP_DUTY:
+  default:
+    pwm_settings.step = STEP_DUTY;
+    lcd_gotoxy(0, 2);
+    lcd_puts("Duty cycle");
+    lcd_gotoxy(0, 4);
+    duty_to_line();
+    lcd_puts(line);
+    lcd_gotoxy(0, 6);
+    lcd_puts("Frequency");
+    lcd_gotoxy(0, 4);
+    lcd_puts(">");
+    disp_draw_mark_position((DUTY_POS_0 - pwm_settings.duty_pos), 4, DOUBLESIZE);
+    break;
+  }
   lcd_display();
 }
